67f803d43be2a2f663a4dbd6f55614f09322f561
Brandon Macias <maciasbrandon@protonmail.com>
2026-01-19 02:13:24 -0500

    - Added additional Instagram btn in footer for shrine's instagram page - Remove temporary Instagram btn from shrine page

--- footer.js
+++ footer.js
@@ -7,6 +7,9 @@ export function Footer(){
     <a href="https://www.instagram.com/sacredheartliv/" target="_blank">
         <i class="bx bxl-instagram"></i>
     </a>
+    <a href="https://www.instagram.com/allsaintsshrinemi/" target="_blank">
+        <i class="bx bxl-instagram"></i>
+    </a>
     <a href="https://www.youtube.com/@EparchyofParma" target="_blank">
         <i class="bx bxl-youtube"></i>
     </a>
